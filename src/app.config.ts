4ce253d9537a0d23380216dbf87126383982d5ae
loganrw <loganrwatkins@hotmail.com>
2025-05-30 19:56:52 -0500

    add logic to disconnect

--- src/app.config.ts
+++ src/app.config.ts
@@ -30,8 +30,8 @@ export default config({
 
         app.post("/leave_room", async (req, res) => {
             let room: MyRoom = JSON.parse(req.body.room) as MyRoom;
-            console.log("ROOM: ", room);
-            //if (room.state.players <= 0) room.disconnect();
+            if (room.state.players <= 0) room.disconnect();
+            // Add logic for the reamining player to win here
             res.sendStatus(200);
         })
 
